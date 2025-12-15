b855e704eb02500cb8894f2d0b0d8a2ebbe6eafd
Ramela M S <ramelams07@gmail.com>
2025-12-15 13:32:22 +0530

    Initial commit

---
+++ Base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+# Create your models here.
+class Contact(models.Model):
+	name 	= models.CharField(max_length = 30, blank = False)
+	email 	= models.EmailField()
+	phone_no= models.CharField(max_length = 10, validators=[RegexValidator(r'^\d{10}$', message="Phone number must be 10 digits")])
+	content = models.TextField(max_length = 300, blank = False)
---
+++ Base/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from .models import Contact
+# Create your views here.
+
+
+
+def add_to_db(request):
+	if request.method == "POST":
+		name 	= request.POST.get("name")
+		email 	= request.POST.get("email")
+		phone_no= request.POST.get("phone_no")
+		content 	= request.POST.get("content")
+
+		cont = Contact(name=name, email=email, phone_no=phone_no, content=content)
+		try:
+			cont.full_clean()
+			cont.save()
+			messages.success(request, "Your Contacts had been Saved successfully!")
+			return redirect("/")
+			
+		except ValidationError as e:
+			for field,errors in e.message_dict.items():
+				for error in errors:
+					messages.error(request, f"{field}: {error}", extra_tags='danger')
+				return redirect("/")
+	messages.success(request, "Welcome to my Portfolio!")
+	return render(request,"Base/home.html")
