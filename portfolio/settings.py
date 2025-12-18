ab014b7b66b5afaa82f90e72705a922e4495407a
Nawin M S <ramelams07@gmail.com>
2025-12-18 12:41:21 +0530

    Created requiremets.txt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,10 +28,9 @@ SECRET_KEY =  os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "ramela-m-s-portfolio.onrender.com"
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.up.railway.app"
 ]
 
 
