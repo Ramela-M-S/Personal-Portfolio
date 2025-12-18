34b2aa3731ab94e83af156b2a29c37285bfb5d79
Nawin M S <ramelams07@gmail.com>
2025-12-18 13:33:51 +0530

    Changed settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,5 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS =[BASE_DIR /  "static",]
+STATICFILES_DIRS =[BASE_DIR / 'base'/ 'static',]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
