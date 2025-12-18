b5e3f7375ede1342cfefa09cf69afcf8b6a9484f
Nawin M S <ramelams07@gmail.com>
2025-12-18 14:25:54 +0530

    Corrected URL

--- Base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path("", views.add_to_db, name = "home"),
-
-   ]
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from Base import views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("Base.urls")),
+    path('', views.add_to_db, name='home'),
 ]
