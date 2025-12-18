bdca5629d3085a97de9db94207aa555d40f230ab
Nawin M S <ramelams07@gmail.com>
2025-12-18 15:27:44 +0530

    Corrected URL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,14 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-default-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = [os.environ.get("RAILWAY_SERVICE_NAME", "*")]
+ALLOWED_HOSTS = ["ramela-m-s-portfolio.up.railway.app",
+]
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.up.railway.app"
+    "https://ramela-m-s-portfolio.up.railway.app",
 ]
 
 
