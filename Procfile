e7aa2ed13260ca23b696e000907f9feaac43ce1d
Nawin M S <ramelams07@gmail.com>
2025-12-18 15:16:22 +0530

    Corrected URL

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput && gunicorn portfolio.wsgi:application
+web: gunicorn portfolio.wsgi:application --bind 0.0.0.0:$PORT
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Base import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.add_to_db, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
